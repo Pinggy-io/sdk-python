cd54da24c0824bd6d347e288bfb1b8a002e884f1
Bishnu <bishnuthakur284@gmail.com>
2025-04-06 23:24:28 +0530

    setup.py wip

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import sys
 import urllib.request
 from setuptools import setup, find_packages
 from setuptools.dist import Distribution
-from setuptools.command.build_py import build_py
 
 
 PLATFORM_FILES = {
@@ -23,57 +22,66 @@ arch_map = {
     "arm64": "aarch64",
 }
 
+def get_shared_libraries():
+    system = None
+    arch = None
+    
+    VERSION = "v0.1"
+    DEFAULT_URL = f"http://127.0.0.1:8000/"
+    BASE_URL = None
+    
+    # target_plat = os.environ.get("TARGET_PLAT")
+    
+    for arg in sys.argv:
+        if arg.startswith("--plat-name="):
+            plat = arg.split("=")[-1].lower()
+            if "-" in plat:
+                system, arch = plat.split("-") 
+            elif "macos" in plat:
+                system, arch = "darwin", "universal"       
+        elif arg.startswith("--lib-server="):
+            BASE_URL = arg.split("=")[-1].lower()
+        
+    if system is None:
+        platform_key = sysconfig.get_platform().lower()
+        system, _,arch = platform_key.partition("-")
+        
+    if BASE_URL is None:
+        BASE_URL = os.environ.get("LIB_PINGGY_SERVER",DEFAULT_URL)
+        
+    arch = arch_map.get(arch, arch) 
+    
+    print(f"platform = {system}\narch={arch}")
+    
+    url = None
+    dest_path = None
+    src = None
+    package_files = []
+    
+    dest_dir = "pinggy/bin"
+    os.makedirs(dest_dir, exist_ok=True)
 
-class CustomBuild(build_py):
-    def run(self):
-        system = None
-        arch = None
-        
-        VERSION = "v0.1"
-        DEFAULT_URL = f"http://127.0.0.1:8000/"
-        BASE_URL = None
-        
-        # target_plat = os.environ.get("TARGET_PLAT")
-        
-        for arg in sys.argv:
-            if arg.startswith("--plat-name="):
-                plat = arg.split("=")[-1].lower()
-                if "-" in plat:
-                    system, arch = plat.split("-") 
-                elif "macos" in plat:
-                    system, arch = "darwin", "universal"       
-            elif arg.startswith("--lib-server="):
-                BASE_URL = arg.split("=")[-1].lower()
-            
-        if system is None:
-            platform_key = sysconfig.get_platform().lower()
-            system, _,arch = platform_key.partition("-")
-            
-        if BASE_URL is None:
-            BASE_URL = os.environ.get("LIB_PINGGY_SERVER",DEFAULT_URL)
-            
-        arch = arch_map.get(arch, arch) 
-        
-        print(f"platform = {system}\narch={arch}")
-        
-        url = None
-        dest_path = None
-        
-        if(system == "linux"):
-            url = f"{BASE_URL}{VERSION}/{system}/{arch}/libpinggy.so"
-            dest_path = os.path.join('pinggy', "libpinggy.so")
-            print(f"[+] Downloading libpinggy.so from {url}")
-        elif(system == "win"):
-            url = f"{BASE_URL}{VERSION}/{system}/{arch}/pinggy.dll"
-            dest_path = os.path.join('pinggy', "pinggy.dll")
-            print(f"[+] Downloading pinggy.dll from {url}")
-        elif(system == "darwin"):
-            url = f"{BASE_URL}{VERSION}/{system}/{arch}/libpinggy.dylib"
-            dest_path = os.path.join('pinggy', "libpinggy.dylib")
-            print(f"[+] Downloading libpinggy.dylib from {url}")
-        
-        urllib.request.urlretrieve(url, dest_path)
-        super().run()
+    
+    if(system == "linux"):
+        src = "libpinggy.so"
+        url = f"{BASE_URL}{VERSION}/{system}/{arch}/libpinggy.so"
+        dest_path = os.path.join(dest_dir, os.path.basename(src))
+        print(f"[+] Downloading libpinggy.so from {url}")
+    elif(system == "win"):
+        src = "pinggy.dll"
+        url = f"{BASE_URL}{VERSION}/{system}/{arch}/pinggy.dll"
+        dest_path = os.path.join(dest_dir, os.path.basename(src))
+        print(f"[+] Downloading pinggy.dll from {url}")
+    elif(system == "darwin"):
+        src = "libpinggy.dylib"
+        url = f"{BASE_URL}{VERSION}/{system}/{arch}/libpinggy.dylib"
+        dest_path = os.path.join(dest_dir, os.path.basename(src))
+        print(f"[+] Downloading libpinggy.dylib from {url}")
+    
+    urllib.request.urlretrieve(url, dest_path)
+    package_files.append(f"bin/{os.path.basename(src)}")
+    return package_files
+
 
 class BinaryDistribution(Distribution):
     def has_ext_modules(self):
@@ -84,11 +92,11 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
-    package_data={"pinggy": ["*.so", "*.dll", "*.dylib"]},
+    package_data={"pinggy": get_shared_libraries()},
     description='Cross-platform shared library package',
     author='Bishnu Thakur',
     license='MIT',
     distclass=BinaryDistribution,
-    cmdclass={'build_py': CustomBuild},
+    # cmdclass={'build_py': CustomBuild},
     zip_safe=False,
 )
