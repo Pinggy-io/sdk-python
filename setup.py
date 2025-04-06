9577da1ebfaeac6e5322a56fd8a8a82c1e3cf327
Bishnu <bishnuthakur284@gmail.com>
2025-04-06 18:31:18 +0530

    task #4 wip

--- setup.py
+++ setup.py
@@ -1,93 +1,84 @@
-from setuptools import setup, find_packages
-import sys
-import platform
-import shutil
 import os
-import re
-from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+import sysconfig
+import sys
+import urllib.request
+from setuptools import setup, find_packages
+from setuptools.dist import Distribution
+from setuptools.command.build_py import build_py
 
-class custom_bdist_wheel(_bdist_wheel):
-    # def __init__(self):
-    #     print("helllllllloooooooooooooooooooooooooooooooooooooo")
-    #     super().__init__()
+VERSION = "v0.1"
+BASE_URL = f"http://127.0.0.1:8000/{VERSION}/"
+
+PLATFORM_FILES = {
+    'linux-x86_64': 'libpinggy.so',
+    'win-amd64': 'libpinggy.dll',
+    'macosx_10_9_x86_64': 'lib.dylib',
+}
+
+arch_map = {
+    "2_28-x86_64":"x86_64",
+    "amd64": "x86_64",
+    "x86_64": "x86_64",
+    "i386": "i686",
+    "win32": "i686",
+    "armv7l": "armv7",
+    "arm64": "aarch64",
+}
+
+
+class CustomBuild(build_py):
     def run(self):
-        print("=====########",self.plat_name)
-        _bdist_wheel.run(self)
+        system = None
+        arch = None
+        
+        for arg in sys.argv:
+            if arg.startswith("--plat-name="):
+                plat = arg.split("=")[-1].lower()
+                if "-" in plat:
+                    system, arch = plat.split("-") 
+                elif "macos" in plat:
+                    system, arch = "darwin", "universal"       
+            else:
+                platform_key = sysconfig.get_platform().lower()
+                system, _,arch = platform_key.partition("-")
+            
+        arch = arch_map.get(arch, arch) 
+        
+        print(f"platform = {platform_key}\narc={arch}")
+        
+        url = None
+        dest_path = None
+        
+        if(system == "linux"):
+            url = f"{BASE_URL}{system}/{arch}/libpinggy.so"
+            dest_path = os.path.join('pinggy', "libpinggy.so")
+            print(f"[+] Downloading libpinggy.so from {url}")
+        elif(system == "win"):
+            url = f"{BASE_URL}{system}/{arch}/pinggy.dll"
+            dest_path = os.path.join('pinggy', "pinggy.dll")
+            print(f"[+] Downloading pinggy.dll from {url}")
+        elif(system == "darwin"):
+            url = f"{BASE_URL}{system}/{arch}/libpinggy.dylib"
+            dest_path = os.path.join('pinggy', "libpinggy.dylib")
+            print(f"[+] Downloading libpinggy.dylib from {url}")
+        
+        urllib.request.urlretrieve(url, dest_path)
+        super().run()
 
-def get_plat_name():
-    """Extract --plat-name from sys.argv"""
-    for arg in sys.argv:
-        match = re.match(r"--plat-name=(.+)", arg)
-        if match:
-            return match.group(1)
-    return None
-
-def get_shared_libraries():
-    print("Fetching shared libraries with args:", sys.argv)
-
-    plat_name = get_plat_name()
-    
-    if plat_name:
-        print(f"Using provided --plat-name: {plat_name}")
-        system, _, machine_folder = plat_name.partition("-")
-        system = system.lower()
-        print(f"system = {system}, machine_folder = {machine_folder}")
-    else:
-        system = platform.system().lower()
-        machine_folder = platform.machine().lower()
-
-
-    arch_map = {
-        "2_28-x86_64":"x86_64",
-        "amd64": "x86_64",
-        "x86_64": "x86_64",
-        "i386": "i686",
-        "win32": "i686",
-        "armv7l": "armv7",
-        "arm64": "aarch64",
-    }
-
-    machine_folder = arch_map.get(machine_folder, machine_folder)
-
-    if system == "manylinux":
-        src_files = [f"pinggy/linux/{machine_folder}/libpinggy.so", f"pinggy/linux/{machine_folder}/pinggyclient"]
-    elif system == "macosx":
-        src_files = ["pinggy/macos/universal/libpinggy.dylib", "pinggy/macos/universal/pinggyclient"]
-    elif system == "win":
-        src_files = [f"pinggy/windows/{machine_folder}/pinggy.dll"]
-    else:
-        # machine_folder = "x86_64"
-        # src_files = [f"pinggy/windows/{machine_folder}/pinggy.dll"]
-        raise RuntimeError(f"Unsupported OS: {system}")
-
-    dest_dir = "pinggy/bin"
-    os.makedirs(dest_dir, exist_ok=True)
-
-    copied_files = []
-    for src in src_files:
-        print(f"copyinggggg ---- {src}")
-        dest = os.path.join(dest_dir, os.path.basename(src))
-        shutil.copy2(src, dest)
-        copied_files.append(f"bin/{os.path.basename(src)}")
-
-    return copied_files
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
 
 setup(
-     cmdclass={
-        'bdist_wheel': custom_bdist_wheel,
-    },
-    name="dev-pinggy",
-    version="1.0.0",
-    author="Bishnu",
-    author_email="bishnuthakur284@gmail.com",
-    description="Pinggy package for tunneling",
+    name='dev-pinggy',
+    version='1.0.0',
     packages=find_packages(),
-    has_ext_modules=lambda: True,
     include_package_data=True,
-    package_data={"pinggy": get_shared_libraries()}, 
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
+    # package_data={'pinggy': },
+    description='Cross-platform shared library package',
+    author='Bishnu Thakur',
+    license='MIT',
+    distclass=BinaryDistribution,
+    cmdclass={'build_py': CustomBuild},
+    zip_safe=False,
 )
