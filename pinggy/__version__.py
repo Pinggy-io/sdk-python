189d8395002fba8d5f3ea63155eae70ec228affe
Abhijit@mac_air <abhijit.manpur@gmail.com>
2025-08-06 21:23:12 +0530

    updating version

--- pinggy/__version__.py
+++ pinggy/__version__.py
@@ -1,3 +1,3 @@
-__lib_pinggy_version = "0.0.17"
+__lib_pinggy_version = "0.0.19"
 __lib_pinggy_url_prefix = "https://github.com/Pinggy-io/libpinggy/releases/download/v"
-__version__ = "v0.0.14"
+__version__ = "v0.0.15"
